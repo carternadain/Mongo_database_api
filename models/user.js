dde857247c3ae19b9f7c33639d8920de8f1733df
carternadain <cnadain@gmail.com>
2022-05-16 20:29:49 -0700

    update user model

--- models/user.js
+++ models/user.js
@@ -1,26 +1,47 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-// const UserSchema = new Schema({
-//     UserName: {
-//       type: String
-//     },
-//     email: {
-//       type: String
-//     },
-//     thoughts: {
-//       type: Date,
-//       default: Date.now
-//     },
-//     friends: {
-//       type: String,
-//       default: 'Large'
-//     },
-//     toppings: []
-//   });
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: "Please input Username",
+    },
 
+    email: {
+      type: String,
+      unique: true,
+      required: "Please input Username",
+      match: [/.+@.+\..+/],
+    },
 
-  // create the Pizza model using the PizzaSchema
-const User = model('User', UserSchema);
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
 
-// export the Pizza model
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
 module.exports = User;
